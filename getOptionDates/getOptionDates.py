91deac7f80bee62fdc79aace967bbb1185c4eb5a
Elias <46169780+Stealthm8@users.noreply.github.com>
2021-02-02 18:08:12 -0500

    Add files via upload

---
+++ getOptionDates/getOptionDates.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as pdr
+import datetime
+from datetime import date
+import yfinance as yf
+yf.pdr_override()
+
+today = date.today()
+ticker = 'SPY'
+
+data = yf.Ticker(ticker)
+dates = data.options
+
+for date in dates:
+	print(date)
