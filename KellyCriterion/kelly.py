e8fa3c1308d4a63a38ef4e80fe05c45e1f11bc18
hedge0 <eliasmemail@gmail.com>
2021-11-04 02:42:12 -0400

    update

--- KellyCriterion/kelly.py
+++ KellyCriterion/kelly.py
@@ -2,34 +2,17 @@ import sys
 import random
 import numpy as np
 
-#check that string can be converted to float
-def checkFloat(val):
-	try:
-		float(val)
-	except ValueError:
-		sys.exit("Not a Float")
-
-	return float(val)
-
-#check that string can be converted to int
-def checkInt(val):
-	try:
-		int(val)
-	except ValueError:
-		sys.exit("Not an Int")
-
-	return int(val)
-
 class Kelly:
 	def __init__(self, probability, risk, reward, runs):
 
-		probability = checkFloat(probability)
-		risk = checkFloat(risk)
-		reward = checkFloat(reward)
-		runs = checkInt(runs)
+		probability = self._check_float(probability)
+		risk = self._check_float(risk)
+		reward = self._check_float(reward)
+		runs = self._check_int(runs)
 
 		ratio = reward / risk
 		kelly = probability - ((1 - probability) / ratio)
+		
 		if(kelly <= 0):
 			sys.exit("Suggested betting size is less than or equal to 0 and should not be taken")
 
@@ -39,17 +22,35 @@ class Kelly:
 		self.ratio = ratio
 		self.kelly = kelly
 		self.runs = runs
-		self.kellySim = self.monteCarlo(True)
-		self.fullSim = self.monteCarlo(False)
+		self.kellySim = self._monte_carlo(True)
+		self.fullSim = self._monte_carlo(False)
 		self.kellyMean = np.mean(self.kellySim)
 		self.fullMean = np.mean(self.fullSim)
 		self.kellySigma = np.std(self.kellySim)
 		self.fullSigma = np.std(self.fullSim)
-		self.kellyWalks = self.getProfitableWalks(self.kellySim)
-		self.fullWalks = self.getProfitableWalks(self.fullSim)
+		self.kellyWalks = self._get_profitable_walks(self.kellySim)
+		self.fullWalks = self._get_profitable_walks(self.fullSim)
 	
+	#check that string can be converted to float
+	def _check_float(self, val):
+		try:
+			float(val)
+		except ValueError:
+			sys.exit("Not a Float")
+
+		return float(val)
+
+	#check that string can be converted to int
+	def _check_int(self, val):
+		try:
+			int(val)
+		except ValueError:
+			sys.exit("Not an Int")
+
+		return int(val)
+
 	#run simulation equal to number of random walks
-	def monteCarlo(self, simType):
+	def _monte_carlo(self, simType):
 		walks = 10000
 		results = []
 		for i in range(walks):
@@ -58,14 +59,14 @@ class Kelly:
 			for i in range(1, self.runs + 1):
 				prev = randomWalk[i-1]
 				if simType:
-					randomWalk.append(self.kellyVal(prev))
+					randomWalk.append(self._kelly_val(prev))
 				else:
-					randomWalk.append(self.fullVal(prev))
+					randomWalk.append(self._full_val(prev))
 			results.append(randomWalk[-1])
 		return results
 
 	#compute new value using kelly
-	def kellyVal(self, prev):
+	def _kelly_val(self, prev):
 		if random.random() < self.probability:
 			kellyVal = (self.kelly * prev) * (1 + self.reward)
 			otherVal = (1 - self.kelly) * prev
@@ -77,7 +78,7 @@ class Kelly:
 		return totalVal
 
 	#compute new value using full
-	def fullVal(self, prev):
+	def _full_val(self, prev):
 		if random.random() < self.probability:
 			totalVal = prev * (1 + self.reward)
 		else:
@@ -85,7 +86,7 @@ class Kelly:
 		return totalVal
 
 	#get percent of random walks that were profitable
-	def getProfitableWalks(self, results):
+	def _get_profitable_walks(self, results):
 		count = 0
 		for value in results:
 			if value > 1:
