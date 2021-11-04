f40ab77705331b0ffeb8e1776612f947023c7926
hedge0 <eliasmemail@gmail.com>
2021-11-04 02:50:16 -0400

    update

--- KellyCriterion/sim.py
+++ KellyCriterion/sim.py
@@ -7,14 +7,14 @@ runs = input("Enter the Number of Consecutive Bets: ")
 
 sim = Kelly(probability, risk, reward, runs)
 
-print(f"\nWin Probability: {sim.getProbability() * 100}%")
-print(f"Risk Reward Ratio: {round(sim.getRatio(), 4)}")
-print(f"Optimal Kelly Size: {round(sim.getKelly(), 4) * 100}%")
+print(f"\nWin Probability: {sim.get_probability() * 100}%")
+print(f"Risk Reward Ratio: {round(sim.get_ratio(), 4)}")
+print(f"Optimal Kelly Size: {round(sim.get_kelly(), 4) * 100}%")
 
-print(f"\nKelly μ: {round(sim.getKellyMean(), 4)}")
-print(f"Kelly σ: {round(sim.getKellySigma(), 4)}")
-print(f"{round(sim.getKellyWalks() * 100, 4)}% Profitable Walks")
+print(f"\nKelly μ: {round(sim.get_kelly_mean(), 4)}")
+print(f"Kelly σ: {round(sim.get_kelly_sigma(), 4)}")
+print(f"{round(sim.get_kelly_walks() * 100, 4)}% Profitable Walks")
 
-print(f"\nFull μ: {round(sim.getFullMean(), 4)}")
-print(f"Full σ: {round(sim.getFullSigma(), 4)}")
-print(f"{round(sim.getFullWalks() * 100, 4)}% Profitable Walks")
+print(f"\nFull μ: {round(sim.get_full_mean(), 4)}")
+print(f"Full σ: {round(sim.get_full_sigma(), 4)}")
+print(f"{round(sim.get_full_walks() * 100, 4)}% Profitable Walks")
