632b7393545aa1656b8ba88df056b84be46ff9e4
Elias Martin <elias@sportsvisio.com>
2021-11-04 02:09:50 -0400

    update

--- KellyCriterion/kelly.py
+++ KellyCriterion/kelly.py
@@ -9,6 +9,8 @@ def checkFloat(val):
 	except ValueError:
 		sys.exit("Not a Float")
 
+	return float(val)
+
 #check that string can be converted to int
 def checkInt(val):
 	try:
@@ -16,112 +18,125 @@ def checkInt(val):
 	except ValueError:
 		sys.exit("Not an Int")
 
-#get probability of winning from user
-def getProbability():
-	probability = input("Enter the Expected Probability of Winning: ")
-	checkFloat(probability)
-	probability = float(probability)
-	return probability
+	return int(val)
 
-#get win loss ratio from user
-def getRatio():
-	reward = input("Enter the Potential Reward: ")
-	checkFloat(reward)
-	reward = float(reward)
-	risk = input("Enter the Potential Risk: ")
-	checkFloat(risk)
-	risk = float(risk)
-	ratio = reward / risk
-	return reward, risk, ratio
+class Kelly:
+	def __init__(self, probability, risk, reward, runs):
 
-#get number of consecutive bets from user
-def getRuns():
-	runs = input("Enter the Number of Consecutive Bets: ")
-	checkInt(runs)
-	runs = int(runs)
-	return runs
+		probability = checkFloat(probability)
+		risk = checkFloat(risk)
+		reward = checkFloat(reward)
+		runs = checkInt(runs)
 
-#get kelly size from formula
-def getKelly(probability, ratio):
-	kelly = (probability - ((1 - probability) / ratio))
-	if(kelly <= 0):
-		sys.exit("Suggested betting size is less than or equal to 0 and should not be taken")
-	return kelly
+		ratio = reward / risk
+		kelly = probability - ((1 - probability) / ratio)
+		if(kelly <= 0):
+			sys.exit("Suggested betting size is less than or equal to 0 and should not be taken")
 
-#get stats from results
-def getStats(results):
-	mean = np.mean(results) 
-	sigma = np.std(results)
-	return mean, sigma
+		self.probability = probability
+		self.risk = risk
+		self.reward = reward
+		self.ratio = ratio
+		self.kelly = kelly
+		self.runs = runs
+		self.kellySim = self.monteCarlo(True)
+		self.fullSim = self.monteCarlo(False)
+		self.kellyMean = np.mean(self.kellySim)
+		self.fullMean = np.mean(self.fullSim)
+		self.kellySigma = np.std(self.kellySim)
+		self.fullSigma = np.std(self.fullSim)
+		self.kellyWalks = self.getProfitableWalks(self.kellySim)
+		self.fullWalks = self.getProfitableWalks(self.fullSim)
+	
+	#run simulation equal to number of random walks
+	def monteCarlo(self, simType):
+		walks = 10000
+		results = []
+		for i in range(walks):
+			randomWalk = []
+			randomWalk.append(1)
+			for i in range(1, self.runs + 1):
+				prev = randomWalk[i-1]
+				if simType:
+					randomWalk.append(self.kellyVal(prev))
+				else:
+					randomWalk.append(self.fullVal(prev))
+			results.append(randomWalk[-1])
+		return results
 
-#compute new value using kelly bet
-def kellyVal(probability, reward, risk, kelly, prev):
-	if random.random() < probability:
-		kellyVal = (kelly * prev) * (1 + reward)
-		otherVal = (1 - kelly) * prev
-		totalVal = kellyVal + otherVal
-	else:
-		kellyVal = (kelly * prev) * (1 - risk)
-		otherVal = (1 - kelly) * prev
-		totalVal = kellyVal + otherVal
-	return totalVal
+	#compute new value using kelly
+	def kellyVal(self, prev):
+		if random.random() < self.probability:
+			kellyVal = (self.kelly * prev) * (1 + self.reward)
+			otherVal = (1 - self.kelly) * prev
+			totalVal = kellyVal + otherVal
+		else:
+			kellyVal = (self.kelly * prev) * (1 - self.risk)
+			otherVal = (1 - self.kelly) * prev
+			totalVal = kellyVal + otherVal
+		return totalVal
 
-#compute new value using full bet
-def fullVal(probability, reward, risk, prev):
-	if random.random() < probability:
-		totalVal = prev * (1 + reward)
-	else:
-		totalVal = prev * (1 - risk)
-	return totalVal
+	#compute new value using full
+	def fullVal(self, prev):
+		if random.random() < self.probability:
+			totalVal = prev * (1 + self.reward)
+		else:
+			totalVal = prev * (1 - self.risk)
+		return totalVal
 
-#run simulation equal to number of random walks
-def monteCarlo(probability, reward, risk, walks, runs, kelly = 0):
-	results = []
-	for i in range(walks):
-		randomWalk = []
-		randomWalk.append(1)
-		for i in range(1, runs + 1):
-			prev = randomWalk[i-1]
-			if kelly > 0:
-				randomWalk.append(kellyVal(probability, reward, risk, kelly, prev))
-			else:
-				randomWalk.append(fullVal(probability, reward, risk, prev))
-		results.append(randomWalk[-1])
-	return results
+	#get percent of random walks that were profitable
+	def getProfitableWalks(self, results):
+		count = 0
+		for value in results:
+			if value > 1:
+				count += 1
+		prof = (count / len(results))
+		return prof
 
-#get percent of random walks that were profitable
-def getProfitableWalks(results):
-	count = 0
-	for value in results:
-		if value > 1:
-			count += 1
-	prof = (count / len(results))
-	return prof
+	#get probability
+	def getProbability(self):
+		return self.probability
 
-#get inputs to run simulation and print results
-def runSim(probability, reward, risk, walks, runs, kelly = 0):
-	if kelly > 0:
-		bet = "Kelly"
-	else:
-		bet = "Full"
+	#get risk
+	def getRisk(self):
+		return self.risk
 
-	results = monteCarlo(probability, reward, risk, walks, runs, kelly)
-	prof = getProfitableWalks(results)
-	mean, sigma = getStats(results)
+	#get reward
+	def getReward(self):
+		return self.reward
 
-	print(f"\n{bet} μ: {round(mean, 4)}")
-	print(f"{bet} σ: {round(sigma, 4)}")
-	print(f"{round(prof * 100, 4)}% Profitable Walks")
+	#get ratio
+	def getRatio(self):
+		return self.ratio
+	
+	#get kelly
+	def getKelly(self):
+		return self.kelly
 
-probability = getProbability()
-reward, risk, ratio = getRatio()
-runs = getRuns()
-walks = 10000
-kelly = getKelly(probability, ratio)
+	#get number of consecutive runs
+	def getRuns(self):
+		return self.runs
 
-print(f"\nWin Probability: {probability * 100}%")
-print(f"Risk Reward Ratio: {round(ratio, 4)}")
-print(f"Optimal Kelly Size: {round(kelly, 4) * 100}%")
+	#get kellyMean
+	def getKellyMean(self):
+		return self.kellyMean
 
-runSim(probability, reward, risk, walks, runs, kelly)
-runSim(probability, reward, risk, walks, runs)
+	#get fullMean
+	def getFullMean(self):
+		return self.fullMean
+
+	#get kellySigma
+	def getKellySigma(self):
+		return self.kellySigma	
+
+	#get fullSigma
+	def getFullSigma(self):
+		return self.fullSigma
+
+	#get kellyWalks
+	def getKellyWalks(self):
+		return self.kellyWalks	
+
+	#get fullWalks
+	def getFullWalks(self):
+		return self.fullWalks
