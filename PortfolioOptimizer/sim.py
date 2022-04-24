57f4f095b792ed6ec350faa69308c50f98dcde99
Elias <eliasmemail@gmail.com>
2022-04-24 10:32:59 -0400

    finished mpt project class conversion

--- PortfolioOptimizer/sim.py
+++ PortfolioOptimizer/sim.py
@@ -4,10 +4,12 @@ def main():
     tickers = ['MSFT', 'TSLA', 'AMZN', 'AAPL']
     sim = MPT(tickers, 2)
     sim.runSimulation(10000)
-    print(f"{'Annualized Returns'}: {round(sim.getReturn() * 100, 2)}%")
-    print(f"{'Annualized Volatility'}: {round(sim.getVolatility() * 100, 2)}%")
+    print(f"Annualized Returns: {round(sim.getReturn() * 100, 2)}%")
+    print(f"Annualized Volatility: {round(sim.getVolatility() * 100, 2)}%")
     print(f"Sharpe Ratio: {round(sim.getSharpe(), 2)}\n")
-    print(sim.getPortfolio())
+    portfolio = sim.getPortfolio()
+    for ticker in portfolio:
+        print(f"{ticker} requires {round(portfolio[ticker] * 100, 2)}% allocation")
 
 
 if __name__ == '__main__':
